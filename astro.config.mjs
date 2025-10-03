d7895ebc26dde8a2e59c263287ba10f71788e2bd
Tobias Melen <tobias.melen@gmail.com>
2025-10-03 22:07:51 +0200

    Change base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  base: process.env.CI ? '/native-scroll-delay' : '',
+  base: '/delayed-native-scroll',
 });
